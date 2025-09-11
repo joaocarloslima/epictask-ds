a0f6f126ecde919d8d6a691ff5d49e51b521f9c1
joaocarloslima <joaocarloslima@me.com>
2025-09-10 22:18:16 -0300

    add validation

--- src/main/java/br/com/etecia/epictask/controller/TaskController.java
+++ src/main/java/br/com/etecia/epictask/controller/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.etecia.epictask.model.Task;
 import br.com.etecia.epictask.repository.TaskRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/tasks")
@@ -36,7 +38,9 @@ public class TaskController {
     }
 
     @PostMapping("/form")
-    public String create(Task task, RedirectAttributes redirect){
+    public String create(@Valid Task task, BindingResult result, RedirectAttributes redirect ){
+        if(result.hasErrors()) return "form";
+
         System.out.println("Cadastrando tarefa..." + task);
         repository.save(task);
         redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
--- src/main/java/br/com/etecia/epictask/model/Task.java
+++ src/main/java/br/com/etecia/epictask/model/Task.java
@@ -4,6 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +22,16 @@ public class Task {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank
     private String title;
+
+    @Size(min = 10, message = "deve ter pelo menos 10 caracteres")
     private String description;
+
+    @Positive
     private int score;
+
+    @Min(0) @Max(100)
     private int status;
     
 }
